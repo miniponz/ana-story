0e2c2514fafefd62fe9ba77beda19f0e693b5fe1
Cara Ponzini <ponzini@gmail.com>
2019-01-20 13:28:35 -0800

    with added styling and text output

---
+++ main.js
@@ -0,0 +1,31 @@
+//make function that prints each story part (four times)
+
+function printPartOne(){
+    var storyPartOne = 'Ana was a normal dog until she mets an evil dude. The evil dude taught her that humans are bad! Then Ana was like "Dude! evil dude, humans are bad! We must fight them!" Then evil dude was like "yeah... Ana Dogg!" Then, the evil dude was like "let\'s go Dogg (no pun intended)!'
+
+    var showStoryPartOne = document.getElementById('part-one');
+
+    showStoryPartOne.textContent = storyPartOne;
+    //need to figure out how to control where text is written
+}
+function printPartTwo(){
+    var storyPartTwo = 'Ana was preparing to master the world. Her plan was to make a lick launcher that stretched and launched her tongue so she could use it to lick all of the life out of the humans. Her plan to make the lick launcher was to build a tongue extender-jig-a-ma-bob, then add a lick intensifier into the cortex of the tongue extender (we don\'t know what any of that meant).'
+
+    var showStoryPartTwo = document.getElementById('part-two');
+
+    showStoryPartTwo.textContent = storyPartTwo;
+}
+function printPartThree(){
+    var storyPartThree = 'After the lick cannon was built, Ana decided to use it on Riley first. Alas, it worked! Riley was laying doge cold on the floor, but Riley was alive. Riley withstood the lick. Ana decided to use it on all of the world! '
+
+    var showStoryPartThree = document.getElementById('part-three');
+
+    showStoryPartThree.textContent = storyPartThree;
+}
+function printPartFour(){
+    var storyPartFour = 'When Riley saw Ana was using the lick machine on all of his town, he decided to attack doge. It was a very dumb move, but whatever. Riley decided to grab Ana\'s tongue and flail it around like Magikarp. He tried his plan, but when he got there, he realized how dumb it was. So, he decided to give doge a bath instead. He called the super-soak company and told them there was a long dog with short legs and a tongue intensifier trying to destroy his town! The super-soak company decided to help. When they arrived, they sprayed Ana with soap, water, and shampoo. She ran away because being washed was her secret weakness\! The end....or maybe not?'
+
+    var showStoryPartFour = document.getElementById('part-four');
+
+    showStoryPartFour.textContent = storyPartFour;
+}
